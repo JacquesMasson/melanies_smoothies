a64e195420d32f2ccad57f5ca16fbdafd636239e
JacquesMssn <55802436+JacquesMasson@users.noreply.github.com>
2025-02-13 13:10:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,11 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        update fruit_options
+        set search_on = 'Apple'
+        where fruit_name = 'Apples';
+
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
